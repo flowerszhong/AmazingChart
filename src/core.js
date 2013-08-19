bd276f7c7643037e6b8e86eb15b80e2fb7c384c5
unknown <flowerszhong@hotmail.com>
2013-08-19 22:16:56 +0800

    add src/core.js

---
+++ src/core.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @authors mzhong (flowerszhong.github.com)
+ * @date    2013-08-19 22:10:42
+ * @version 0.1
+ */
+
+
+define(function (require) {
+	var d3 = require("d3");
+	var core = function () {
+		
+	}
+
+	core.prototype = {
+
+	}
+
+	return core;
+})
