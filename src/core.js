fc0724c16e75ef9c26526ad145636ca5af58a396
unknown <flowerszhong@hotmail.com>
2013-08-20 23:28:19 +0800

    add init for core.js

--- src/core.js
+++ src/core.js
@@ -1,20 +1,61 @@
 /**
- * 
+ * base chart
  * @authors mzhong (flowerszhong.github.com)
  * @date    2013-08-19 22:10:42
  * @version 0.1
  */
 
+(function(window,undefined){
 
-define(function (require) {
-	var d3 = require("d3");
-	var core = function () {
+// var d3 = require("d3");
+// var $ = require("jquery");
+var $ = window.$,
+	 d3 = d3;
+
+var __defaults = {
+	margin : [5,5,40,40],//top,right,bottom,left
+	axes : [{
+		orient : "left",
+		type : "linear"
+	},{
+		orient : "bottom",
+		type : "time"
+	}],
+	layout : ["back","main","mask"]
+}
+
+var chart = function (el) {
+	this.$el = $(el)
+	this.init();
+};
+
+chart.prototype = {
+	init : function () {
+		var $el = this.$el,
+			width = $el.width(),
+			height = $el.height;
+
+		this._layoutSize = {
+			w : width,
+			h : height
+		}
+		this.svg_el = d3.select($el.get(0)).append("svg")
+			.attr("width",width)
+			.attr("height",height)
+			.append("g");
+
+		this.initLayout();
+	},
+	initLayout : function () {
 		
 	}
+}
 
-	core.prototype = {
 
-	}
+chart.extend = function (protoProps) {
+	// body...
+}
 
-	return core;
-})
+window.chartBase = chart;
+
+})(this);
---
+++ src/historicalbar.js
@@ -0,0 +1,7 @@
+/**
+ * historic-chart.js
+ * @authors mzhong (flowerszhong.github.com)
+ * @date    2013-08-19 22:17:01
+ * @version 0.1
+ */
+
